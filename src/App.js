c517e31470580cf1230156c4d348f23b10ed4a27
kangji <alleneffort1994@gmail.com>
2023-10-18 18:24:11 +0900

    top-nav, init setting

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { createTheme, ThemeProvider, Typography } from "@mui/material";
+import Header from "./components/Header/Header";
 
+const theme = createTheme({
+  spacing: 8,
+  typography: {
+    fontFamily: "'Noto Sans KR', sans-serif",
+  },
+});
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Header />
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/Header/Header.js
@@ -0,0 +1,68 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import { createTheme } from "@mui/material";
+import { ThemeProvider as MuiThemeProvider } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#ffffff", // 원하는 배경 색상으로 변경
+    },
+    text: {
+      primary: "#000000", // 흰색 글자로 변경
+    },
+  },
+});
+
+export default function Header() {
+  return (
+    <MuiThemeProvider theme={theme}>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          width: "100%",
+        }}
+      >
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 20, ml: 30 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <div>
+              <Button color="inherit" sx={{ mr: 8, fontSize: "1.2rem" }}>
+                소개
+              </Button>
+              <Button color="inherit" sx={{ mr: 8, fontSize: "1.2rem" }}>
+                게시판
+              </Button>
+              <Button color="inherit" sx={{ mr: 8, fontSize: "1.2rem" }}>
+                로그인
+              </Button>
+              <Button color="inherit" sx={{ mr: 8, fontSize: "1.2rem" }}>
+                스토어
+              </Button>
+            </div>
+
+            <Button color="inherit" sx={{ ml: 20, mr: 5, fontSize: "1.2rem" }}>
+              문의하기
+            </Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </MuiThemeProvider>
+  );
+}
